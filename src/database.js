c2a803117f2182aa701b8f521e10940cf54be084
Jose <cote99salamanca@gmail.com>
2020-06-10 00:59:31 -0400

    configuracion mysql para heroku

--- src/database.js
+++ src/database.js
@@ -1,10 +1,15 @@
 const mysql = require('mysql');
 //conexion a bd
+
+/*ba9580d1cf9a56 = user
+:425c9d47 = pass
+@us-cdbr-east-05.cleardb.net = host
+/heroku_bb72c7fbf7e6685 = database*/
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'db_portafolio'
+    host: 'us-cdbr-east-05.cleardb.net',
+    user: 'ba9580d1cf9a56',
+    password: '425c9d47',
+    database: 'heroku_bb72c7fbf7e6685'
 });
 connection.connect(error => {
     if(error){
